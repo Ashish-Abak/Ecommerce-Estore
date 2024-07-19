5e30e0695712e59b35f73568f83db2d72ad61b06
Ashish <ashishabak4@gmail.com>
2024-07-19 10:56:19 +0530

    changes

--- ecommapp/urls.py
+++ ecommapp/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home',views.home),
+    path('home/',views.home),
     path('all',views.all),
     path('catfilter/<cv>',views.catfilter),
     path('range',views.range),
